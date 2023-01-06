7728038e1fd3fde6a6280a509839419c3a6184e8
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2023-01-07 03:33:08 +0900

    project initial commit (#1)

---
+++ backend/src/test/java/com/dongjae/nearChatProejct/NearChatProejctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongjae.nearChatProejct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearChatProejctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
